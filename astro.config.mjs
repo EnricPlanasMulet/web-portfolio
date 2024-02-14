207f582552f9282e972a8e8c109ec06b59471410
EnricPlanasMulet <82574299+EnricPlanasMulet@users.noreply.github.com>
2024-02-14 02:12:42 +0100

    FIX: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  base: '/web-portfolio',
   site: 'https://enricplanasmulet.github.io',
   integrations: [tailwind()]
 });
